1a1cc6dbfb38bdc6c250bee81ebda3ac5b14ac73
STimour <st.sayfoutdinov@icloud.com>
2023-11-10 16:16:01 +0100

    modification des fonctionnaité pour une meilleur gestion des équations

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,54 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState("");
-  const numbTable = ["1","2","3","4","5","6","7","8","9", "0", "00"]
-  const signTable = ["+", "-", "*", "/", "."]
+  
+  const numbTable = ["1","2","3","4","5","6","7","8","9", "0"]
+  const signTable = ["+", "-", "%", "x", "/", "."]
 
   const handleNumbClick = (Numb: string) => { 
     setCount(count + Numb);
-  };
+    if(count[0] === '0' && count[1] != "."){
+      setCount(count + "." + Numb)  
+    }else if(count[1] === '.'){
+      setCount(count + Numb)
+    }    
+  }
 
   const ac = () => {
     setCount("");
-  };
+  }
+
+  const backSpace = () => {
+    setCount(count.slice(0, count.length - 1))
+  }
 
   const egal = () => {
-    try {
-      const result = eval(count)
-      setCount(result) 
-    } catch (error) {
-      setCount("Erreur")
-     }
+    const [drt, gch] = ""
+    if(count.includes("/")){
+      const [drt, gch] = count.split("/")
+      console.log(drt)
+      console.log(gch);
+    }else if(count.includes("x")){
+      const [drt, gch] = count.split("x")
+      console.log(drt)
+    console.log(gch);
+    }else if(count.includes("-")){
+      const [drt, gch] = count.split("-")
+      console.log(drt)
+    console.log(gch);
+    }else if(count.includes("+")){
+      const [drt, gch] = count.split("+")
+      console.log(drt)
+    console.log(gch);
+    }
+    //try {
+    //  const result = eval(count)
+    //  setCount(result) 
+    //  result === 0 ? setCount(result + '.') : setCount(result)
+//
+    //} catch (error) {
+    //  setCount("Erreur")
+    // }    
   };
 
   return (
@@ -43,6 +73,7 @@ function App() {
                 </button>
                 ))}
                 <button className='numSig' onClick={ac}>AC</button>
+                <button className='numSig' onClick={backSpace}>C</button>
             </div>
 
             <div className="chiffreButtons">
